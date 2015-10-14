fac791ab70871fca1eef589e41c65cb2286cf092
pedro <pa.favuzzi@gmail.com>
2015-10-14 17:59:23 +0200

    small changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
 #load "./seeds/seed_medias.rb"
+
 Media.destroy_all
 
 _vimeo_media=[
-	{type:"stretch", position:"up",  location:"unknown", link: "https://vimeo.com/142349746"},
-	{type:"fit",     position:"up",  location:"unknown", link: "https://vimeo.com/142284568"},
-	{type:"stretch", position:"sit", location:"unknown", link: "https://vimeo.com/142160136"},
-	{type:"fit",     position:"sit", location:"unknown", link: "https://vimeo.com/142130830"}]
+	{type:"stretch", position:"up",  location:"unknown", link: "https://player.vimeo.com/video/142349746"},
+	{type:"fit",     position:"up",  location:"unknown", link: "https://player.vimeo.com/video/142284568"},
+	{type:"stretch", position:"sit", location:"unknown", link: "https://player.vimeo.com/video/142160136"},
+	{type:"fit",     position:"sit", location:"unknown", link: "https://player.vimeo.com/video/142130830"}
+							]
 
 _vimeo_media.each do |dat_|
 	Media.create(dat_)
